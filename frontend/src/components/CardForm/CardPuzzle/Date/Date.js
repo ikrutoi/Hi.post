45f85178357f53cc18ae1fd1d9b0b566effccd29
Ihar Krutoi <iharkrutoi@gmail.com>
2025-01-02 23:03:17 +0300

    date. added functions for scrolling months forward / backward

--- frontend/src/components/CardForm/CardPuzzle/Date/Date.js
+++ frontend/src/components/CardForm/CardPuzzle/Date/Date.js
@@ -11,15 +11,44 @@ const Date = () => {
     day: currentDate.currentDay,
   })
 
+  const scrollMonthMinus = () => {
+    if (selectedDate.month > 0) {
+      setSelectedDate((state) => {
+        return { ...state, month: selectedDate.month - 1 }
+      })
+    }
+    if (selectedDate.month === 0) {
+      setSelectedDate((state) => {
+        return { ...state, month: 11, year: selectedDate.year - 1 }
+      })
+    }
+  }
+  const scrollMonthPlus = () => {
+    if (selectedDate.month < 11) {
+      setSelectedDate((state) => {
+        return { ...state, month: selectedDate.month + 1 }
+      })
+    }
+    if (selectedDate.month === 11) {
+      setSelectedDate((state) => {
+        return { ...state, month: 0, year: selectedDate.year + 1 }
+      })
+    }
+  }
+
   return (
     <div className="date">
       <form className="date-form">
         <div className="date-header">
-          <div className="header-sign">Left</div>
+          <div className="header-sign" onClick={scrollMonthMinus}>
+            Left
+          </div>
           <div className="header-date">
             <CurrentDateTime selectedDate={selectedDate} />
           </div>
-          <div className="header-sign">Right</div>
+          <div className="header-sign" onClick={scrollMonthPlus}>
+            Right
+          </div>
         </div>
         <div className="date-slider">
           <span className="slider-line"></span>
