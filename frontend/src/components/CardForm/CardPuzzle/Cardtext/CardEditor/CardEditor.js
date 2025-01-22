e96083365faa4634cdc816e37a77107aca6a6e22
Ihar Krutoi <iharkrutoi@gmail.com>
2025-01-22 16:49:50 +0300

    cardtext. Disable the use of the end-of-text mark on the last line. Track the length of the text on the last line.

--- frontend/src/components/CardForm/CardPuzzle/Cardtext/CardEditor/CardEditor.js
+++ frontend/src/components/CardForm/CardPuzzle/Cardtext/CardEditor/CardEditor.js
@@ -38,108 +38,26 @@ const CardEditor = () => {
   const [editable, setEditable] = useState(null)
   const editorRef = useRef(null)
   const editableRef = useRef(null)
+  const [editableWidth, setEditableWidth] = useState(null)
+  useEffect(() => {
+    if (!editableWidth) {
+      setEditableWidth(editableRef.current.offsetWidth)
+    }
+  }, [editableWidth])
   const [linesCount, setLinesCount] = useState(1)
   const [focusedElement, setFocusedElement] = useState(null)
-  const [heightLine, setHeightLine] = useState(null)
+  const [maxLines, setMaxLines] = useState(null)
+  const [isMaxLines, setIsMaxLines] = useState(false)
 
-  // !!!
-  // const getLastElementInLine = () => {
-  //   const elemEditable = document.querySelector(`[data-slate-node="value"]`)
-  //   if (elemEditable) {
-  //     const lastLine = elemEditable.lastElementChild
-  //     const findDeepestElement = (el) => {
-  //       let deepestElement = el
-  //       let deepestLevel = 0
-  //       const traverse = (currentElement, currentLevel) => {
-  //         if (currentLevel > deepestLevel) {
-  //           deepestLevel = currentLevel
-  //           deepestElement = currentElement
-  //         }
-  //         Array.from(currentElement.children).forEach((child) => {
-  //           traverse(child, currentLevel + 1)
-  //         })
-  //       }
-  //       traverse(el, 0)
-  //       return deepestElement
-  //     }
-  //     const deepestElement = findDeepestElement(lastLine)
-  //     console.log('The most recent nested element:', deepestElement)
-  //     return deepestElement
-  //   }
-  // }
-  // !!!
+  const [lastKey, setLastKey] = useState(null)
 
   //  !!!
   const handleSlateChange = (newValue) => {
+    // console.log('last key from Change', lastKey)
     setValue(newValue)
-
-    // const linesCountNow = newValue.filter(
-    //   (node) => node.type === 'paragraph'
-    // ).length
-    // if (linesCount !== linesCountNow) {
-    //   setLinesCount(linesCountNow)
-    // }
   }
 
-  // const handleSelectionChange = () => {
-  //   const { selection } = editor
-  //   if (selection) {
-  //     const [start] = Range.edges(selection)
-  //     const [node] = Editor.node(editor, start)
-  //     setFocusedElement(node)
-  //     // console.log('Фокус на элементе:', node)
-  //     console.log(
-  //       'HandleSelectionChange. Focus path / focus:',
-  //       start.path,
-  //       '/',
-  //       start.offset
-  //     )
-  //     // console.log('Focus node.length:', node.text.length)
-  //   } else {
-  //     setFocusedElement(null)
-  //     console.log('HandleSelectionChange. Элемент потерял фокус')
-  //   }
-  // }
-
-  // const updateSelectionEditable = () => {
-  //   const updateSelection = () => {
-  //     const { selection } = editor
-  //     if (selection) {
-  //       const [start] = Range.edges(selection)
-  //       const [node] = Editor.node(editor, start)
-  //       console.log('node', node)
-  //       // setFocusedElement(node)
-  //       console.log(
-  //         'UseEffect. Focus path / focus:',
-  //         start.path,
-  //         '/',
-  //         start.offset
-  //       )
-  //     }
-  //   }
-  //   document.addEventListener('selectionchange', updateSelection)
-  //   return () => {
-  //     document.removeEventListener('selectionchange', updateSelection)
-  //   }
-  // }
-
-  // useEffect(() => {
-  //   // const updateSelection = () => {
-  //   const { selection } = editor
-  //   if (selection) {
-  //     const [start] = Range.edges(selection)
-  //     const [node] = Editor.node(editor, start)
-  //     setFocusedElement({ node: node, start: start })
-  //     console.log('focus', start.offset)
-  //   }
-  //   // }
-  //   // document.addEventListener('selectionchange', updateSelection)
-  //   // return () => {
-  //   //   document.removeEventListener('selectionchange', updateSelection)
-  //   // }
-  // }, [editor])
-
-  const calcLinesInEditable = () => {
+  const calcLinesInEditable = (linesCount) => {
     if (editorRef.current && editableRef.current) {
       const heightEditor = editorRef.current.offsetHeight
       const heightEditable = editableRef.current.offsetHeight
@@ -147,187 +65,57 @@ const CardEditor = () => {
         '[data-slate-node="element"]'
       )
       const heightLine = nodeElementLine.offsetHeight
-      const linesMax = heightEditor / heightLine
-      const heightLineToFixed = linesMax.toFixed(1)
-      setHeightLine(heightEditable)
-      return { heightLineToFixed }
+      const maxNumberLinesFraction = heightEditor / heightLine
+      const maxNumberLines = Math.floor(maxNumberLinesFraction)
+      setMaxLines(maxNumberLines)
+      if (linesCount >= maxNumberLines) {
+        setIsMaxLines(true)
+      }
+    }
+  }
+
+  const handleKeyDown = (evt) => {
+    setLastKey(evt.key)
+    if (isMaxLines) {
+      console.log('STOP!!')
+      // evt.preventDefault()
     }
   }
 
   const handleKeyUp = (evt) => {
-    // const linesEditable = getSizeEditorAndEditable().linesEditable
-    // const linesMax = getSizeEditorAndEditable().linesMax
-    if (evt.key === 'ArrowRight' || evt.key === 'ArrowLeft') {
-      const { selection } = editor
-      if (selection) {
-        const [start] = Range.edges(selection)
-        const [node] = Editor.node(editor, start)
-        setFocusedElement({ node: node, start: start })
-      }
-    }
-
-    if (evt.key === 'Enter') {
-      console.log('key press Enter')
-      // const marks = markElementsWithId(editor, markId.current)
-      const pathMark = findPathById(editor, markId.current)
-      // const newPathMark = pathMark.map((el, i) => {
-      //   if (i === 0) {
-      //     return (el = el + 1)
-      //   }
-      //   return el
-      // })
-      // removeDuplicateMark(editor, marks)
-      // for (let i = 0; i < marks.length; i++) {
-      // console.log('REMOVE!!')
-      // const { path } = marks[i]
-      Transforms.removeNodes(editor, { at: pathMark })
-      // insertMarkInLastLine()
-      // console.log('MARKS:', marks[i])
-      // }
-      // for (let i = 0; i < marks.length; i++) {
-      //   // console.log('REMOVE!!')
-      //   const { path } = marks[i]
-      //   Transforms.removeNodes(editor, { at: path })
-      //   // console.log('MARKS:', marks[i])
-      // }
-
-      // evt.preventDefault()
-      // Transforms.insertNodes(editor, { text: '@' }, { at: [1, 0] })
-      // const [start] = Range.edges(selection)
-      // const path = findPathById(editor, markId.current)
-      // if (path && Path.equals(start.path, path)) {
-      //   // Transforms.insertNodes(editor, { text: '<>' }, { at: path })
-      //   // Transforms.insertNodes(editor, { text: '\uFEFF' }, { at: path })
-      // }
-      // }
-    }
-    // if (evt.key === 'Enter' && linesEditable >= linesMax) {
-    // console.log('Enter stop')
-    // evt.preventDefault()
-    // if (evt.key === 'Enter' && linesEditable <= linesMax) {
-    //   const [match] = Editor.nodes(editor, {
-    //     match: (n) => Editor.isBlock(editor, n),
-    //   })
-    //   if (match) {
-    //     Transforms.insertNodes(editor, {
-    //       type: 'paragraph',
-    //       children: [{ text: '' }],
-    //     })
-    //   }
-    // }
-  }
-
-  //  !!!
-  const markId = useRef(uuidv4())
-
-  const insertMarkInLastLine = useCallback(() => {
     const { selection } = editor
     if (selection) {
       const [start] = Range.edges(selection)
       const [node] = Editor.node(editor, start)
       setFocusedElement({ node: node, start: start })
-      // setOffset(start.offset)
-      // console.log('INSERT MARK. path / focus:', start.path, '/', start.offset)
-      // console.log('Focus node:', node)
-
-      // const mark = getMarkElementsWithId(editor, markId.current)
-      // console.log('mark', mark[0].path)
     }
-
-    const lastLinePath = [editor.children.length - 1]
-    const [lastLine] = Editor.node(editor, lastLinePath)
-    const endOfLastLinePath = [...lastLinePath, lastLine.children.length]
-    const existingMark = lastLine.children.find(
-      (child) => child.id === markId.current && child.text === '*'
-    )
-    const marks = getMarkElementsWithId(editor, markId.current)
-    if (!existingMark) {
-      // console.log('insert Mark')
-      // Transforms.insertNodes(
-      //   editor,
-      //   {
-      //     type: 'paragraph',
-      //     children: [{ text: '\uFEFF' }],
-      //   },
-
-      //   { at: endOfLastLinePath }
-      // )
-      Transforms.insertNodes(
-        editor,
-        {
-          type: 'mark',
-          children: [{ text: '*', id: markId.current }],
-        },
-
-        { at: endOfLastLinePath }
-      )
-    }
-    if (marks.length > 1) {
-      removeDuplicateMark(editor, marks)
-    }
-
-    // const pathMark = findPathById(editor, markId.current)
-    // Transforms.select(editor, {
-    //   anchor: Editor.before(editor, pathMark),
-    //   focus: Editor.before(editor, pathMark),
-    // })
-  }, [editor])
-
-  const findPathById = (editor, id) => {
-    for (const [node, path] of Editor.nodes(editor, {
-      at: [],
-      match: (n) => n.id === markId.current,
-    })) {
-      return path
-    }
-    return null
-  }
-
-  const getMarkElementsWithId = (editor, id) => {
-    const marks = []
-    for (const [node, path] of Editor.nodes(editor, {
-      at: [],
-      match: (n) => n.id === id,
-    })) {
-      marks.push({ node, path })
-    }
-    return marks
-  }
-
-  const removeDuplicateMark = (editor, marks) => {
-    for (let i = 0; i < marks.length; i++) {
-      // console.log('REMOVE!!')
-      const { path } = marks[i]
-      Transforms.removeNodes(editor, { at: path })
-      // console.log('MARKS:', marks[i])
-    }
-    // marks.splice(0, marks.length)
   }
 
   useEffect(() => {
-    calcLinesInEditable()
+    calcLinesInEditable(linesCount)
   }, [linesCount])
 
   useEffect(() => {
-    insertMarkInLastLine()
-  }, [editor, insertMarkInLastLine, value])
+    if (isMaxLines) {
+      const [lastLineNode, lastLinePath] = Editor.last(editor, [])
+      const domNodeLastLine = ReactEditor.toDOMNode(editor, lastLineNode)
+      const widthLastLine = domNodeLastLine.offsetWidth
+      console.log('widthLine', widthLastLine, 'widthEditor', editableWidth)
+      // console.log('key', lastKey)
+      if (widthLastLine >= editableWidth) {
+        console.log('-------------')
+      }
+    }
 
-  useEffect(() => {
-    const nodesElementValue = Editor.nodes(editor, {
-      at: [],
-      match: (n) => n['data-slate-node'] === 'element',
-    })
-    console.log('nodeSlate', nodesElementValue)
-    const nodesArrayElementValue = Array.from(nodesElementValue)
-    console.log('nodeSlate2', nodesArrayElementValue)
-    console.log('lines//', editor.children.length)
-    // if (
-    //   editorRef.current &&
-    //   editableRef.current &&
-    //   linesCount !== editableRef.current.children.length
-    // ) {
-    //   setLinesCount(editorRef.current.children.length)
-    // }
+    const currentNumberLines = editor.children.length
+    if (linesCount !== currentNumberLines) {
+      if (currentNumberLines === maxLines) {
+        setIsMaxLines(true)
+      } else {
+        setIsMaxLines(false)
+      }
+      setLinesCount(editor.children.length)
+    }
     if (focusedElement) {
       console.log(
         'FOCUS',
@@ -337,7 +125,6 @@ const CardEditor = () => {
         '/length:',
         focusedElement.node.text.length
       )
-      console.log('lines**:', linesCount)
     }
     if (
       focusedElement &&
@@ -346,7 +133,15 @@ const CardEditor = () => {
     ) {
       console.log('End of element on last line!')
     }
-  }, [focusedElement, editor, linesCount])
+  }, [
+    focusedElement,
+    editor,
+    linesCount,
+    maxLines,
+    value,
+    isMaxLines,
+    editableWidth,
+  ])
 
   return (
     <div className="cardeditor">
@@ -360,6 +155,7 @@ const CardEditor = () => {
           <Editable
             ref={editableRef}
             onBlur={() => setFocusedElement(null)}
+            onKeyDown={handleKeyDown}
             onKeyUp={handleKeyUp}
           />
         </Slate>
